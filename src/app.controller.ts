71029d0dc84b255e8663b659c7e406bc4478dc84
Aleksey Kotylev <leksrus>
2023-09-30 21:27:58 -0300

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Post, Logger, Body } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('app')
+export class AppController {
+  private readonly _logger = new Logger(AppController.name);
+  private postData: string;
+
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  public async getPostData(): Promise<string> {
+    return this.postData;
+  }
+
+  @Post()
+  public async postHook(@Body() json: any): Promise<string> {
+    this._logger.log(`...Logging json from hook ${JSON.stringify(json)}`);
+    this.postData = JSON.stringify(json);
+    return 'Notification arrived';
+  }
+}
