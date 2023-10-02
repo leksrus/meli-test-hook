b0e3c12d8453dc2a9fb38a203c23b3e6ff127415
Oleksiy Kotylev <aleks_rus@hotmail.com>
2023-10-02 17:42:50 -0300

    Update app.controller.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Logger, Body, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Logger, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('app')
